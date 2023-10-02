fb28586134c78b3b0542c81acb046b93b490488b
ipjayawick <ipjayawick@gmail.com>
2023-10-03 00:13:53 +0530

    uri changed

--- src/config.jsx
+++ src/config.jsx
@@ -1,7 +1,7 @@
 const config = {
   // apiUrl: 'https://theatre-reservation-backend.onrender.com/api',
   // apiUrl: 'http://localhost:8400/api',
-  apiUrl: '3.238.186.205:8400/api',
+  apiUrl: 'http://3.238.186.205:8400/api',
 
 };
 
